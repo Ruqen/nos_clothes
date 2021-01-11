d7eefeda7be4ae339f86aae6d11525d7be380fa9
Ruqen <73693641+Ruqen@users.noreply.github.com>
2021-01-11 20:33:42 +0300

    oof leftover code

--- cl.lua
+++ cl.lua
@@ -9,10 +9,6 @@ end)
 
 local sleep = 1000
 
-RegisterCommand("31", function()
-    print(GetEntityCoords(PlayerPedId()))
-end)
-
 Citizen.CreateThread(function()
     while true do
         for k,v in pairs(Ruq.locations) do
@@ -117,40 +113,4 @@ function DrawText3D(x,y,z, text)
     DrawText(_x,_y)
     local factor = (string.len(text)) / 370
     DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
-end
-
-RegisterCommand("tpm", function(source)
-    TeleportToWaypoint()
-end)
-
-TeleportToWaypoint = function()
-    --ESX.TriggerServerCallback("esx_marker:fetchUserRank", function(playerRank)
-        --if playerRank == "admin" or playerRank == "superadmin" or playerRank == "mod" then
-            local WaypointHandle = GetFirstBlipInfoId(8)
-
-            if DoesBlipExist(WaypointHandle) then
-                local waypointCoords = GetBlipInfoIdCoord(WaypointHandle)
-
-                for height = 1, 1000 do
-                    SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
-
-                    local foundGround, zPos = GetGroundZFor_3dCoord(waypointCoords["x"], waypointCoords["y"], height + 0.0)
-
-                    if foundGround then
-                        SetPedCoordsKeepVehicle(PlayerPedId(), waypointCoords["x"], waypointCoords["y"], height + 0.0)
-
-                        break
-                    end
-
-                    Citizen.Wait(5)
-                end
-
-                ESX.ShowNotification("Teleported.")
-            else
-                ESX.ShowNotification("Please place your waypoint.")
-            end
-        --else
-            --ESX.ShowNotification("You do not have rights to do this.")
-        --end
-    --end)
 end
